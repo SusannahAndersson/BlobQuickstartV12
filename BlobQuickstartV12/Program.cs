c3474d516190ca14b012a11c4c1bcdd0eda74205
SusannahAndersson <susannah.alx@gmail.com>
2022-09-16 10:43:00 +0200

    added Azure.Identity package

--- BlobQuickstartV12/Program.cs
+++ BlobQuickstartV12/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
